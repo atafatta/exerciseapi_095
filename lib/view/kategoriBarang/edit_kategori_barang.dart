41288156ac713834c3bc543e02e1de054c6868f5
atafatta <atafatta@gmail.com>
2023-05-13 13:48:27 +0700

    menambahkan kelas dan fungsi edit kategori barang

---
+++ lib/view/kategoriBarang/edit_kategori_barang.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class EditKategoriBarang extends StatelessWidget {
+  const EditKategoriBarang({super.key, required this.kategoriBarang});
+
+  final String kategoriBarang;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
